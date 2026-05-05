fd1bc2d6ac7d637d67bd74d4ec235ac74b0a96a6
Yasser Errerhaoui <yassererrerhaoui@gmail.com>
2026-05-05 13:53:44 +0200

    Add MilkRiseAlert app files

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "milk-rise-alert-v1";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css?v=1",
+  "./app.js?v=1",
+  "./manifest.webmanifest?v=1",
+  "./assets/icon.svg",
+  "./assets/apple-touch-icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
